09016288105edd635ea6ff14f5d69320d0790766
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-08-07 04:37:04 +0900

    Add: View 파일 추가

---
+++ NetflixClone/View/PosterCell.swift
@@ -0,0 +1,8 @@
+//
+//  PosterCell.swift
+//  NetflixClone
+//
+//  Created by 김동현 on 8/7/24.
+//
+
+import Foundation
---
+++ NetflixClone/View/YoutubeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  YoutubeViewController.swift
+//  NetflixClone
+//
+//  Created by 김동현 on 8/7/24.
+//
+
+import Foundation
