4d2dcc52432cde005192da57ebf797f68797ede5
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-08-07 17:01:01 +0900

    Add: NetworkManager 파일 추가

---
+++ NetflixClone/Common/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  NetflixClone
+//
+//  Created by 김동현 on 8/7/24.
+//
+
+import Foundation
+
+class NetworkManager {
+  static let shared = NetworkManager()
+  private init() {}
+  
+  
+}
