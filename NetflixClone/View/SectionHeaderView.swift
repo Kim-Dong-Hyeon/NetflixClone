1120690cb0a30a3e21eb51716bd880a9ab6ad572
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-08-07 15:30:07 +0900

    Feat: View 구현

--- NetflixClone/View/SectionHeaderView.swift
+++ NetflixClone/View/SectionHeaderView.swift
@@ -5,4 +5,38 @@
 //  Created by 김동현 on 8/7/24.
 //
 
-import Foundation
+import UIKit
+
+class SectionHeaderView: UICollectionReusableView {
+  static let id = "SectionHeaderView"
+  
+  let titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.boldSystemFont(ofSize: 18)
+    return label
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupUI() {
+    addSubview(titleLabel)
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+      titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+      titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+      titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5)
+    ])
+  }
+  
+  func configure(with title: String) {
+    titleLabel.text = title
+  }
+}
